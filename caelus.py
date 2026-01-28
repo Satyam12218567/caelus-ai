7d3b34fdd0ddad5c86cbd0dcdb3075ff9a07c437
Satyam Raj <119718503+Satyam12218567@users.noreply.github.com>
2026-01-29 03:08:42 +0530

    Update caelus.py

--- caelus.py
+++ caelus.py
@@ -17,7 +17,7 @@ except FileNotFoundError:
     st.stop()
 
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-001")
 
 if "history" not in st.session_state:
     st.session_state.history = []
